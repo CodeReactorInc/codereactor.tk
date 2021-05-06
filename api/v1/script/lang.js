48291d8e778da148aeed7d7ac83da379f82e023f
KindredNatsu <velocicraft2088@gmail.com>
2021-05-06 19:48:44 -0300

    Fix script overwrite

--- api/v1/script/lang.js
+++ api/v1/script/lang.js
@@ -7,7 +7,14 @@ function getBasicLangCode() {
 }
 
 function loadLang(forceEnglish) {
+    let oldlangnode = document.getElementById("loadedlang");
+    if (oldlangnode != null) {
+        document.querySelector("head").removeChild(oldlangnode);
+        delete oldlangnode;
+    }
+
     let langnode = document.createElement("script");
+    langnode.id = "loadedlang";
     if (forceEnglish === true) langnode.src = "https://codereactor.tk/api/v1/lang/en.js";
     else langnode.src = "https://codereactor.tk/api/v1/lang/" + getBasicLangCode() + ".js";
     document.querySelector("head").appendChild(langnode);
