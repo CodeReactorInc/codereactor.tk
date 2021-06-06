15ebbe2570785a69dc8004f9faa43029aeabdf11
NashiraDeer <velocicraft2088@gmail.com>
2021-06-06 13:57:09 -0300

    Reduce 1 byte

--- api/v1/script/mobile.js
+++ api/v1/script/mobile.js
@@ -1,5 +1,5 @@
 function isUserAgentMobile() {
-  return navigator.userAgent.match(MOBILE_USER_AGENT_REGEX) !== null;
+  return navigator.userAgent.match(MOBILE_USER_AGENT_REGEX) != null;
 }
 
 const MOBILE_USER_AGENT_REGEX = /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|NetFront|Silk-Accelerated|(hpw|web)OS|Fennec|Minimo|Opera M(obi|ini)|Blazer|Dolfin|Dolphin|Skyfire|Zune/g;
